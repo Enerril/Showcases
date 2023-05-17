00d0b51fd85a8bf474cba80e54a4541d213a7008
SpiralBeam <32187189+Enerril@users.noreply.github.com>
2023-05-17 22:42:07 +0300

    another intermediate
    spent ~5 hours learning about node canvas and another ~5 improving space partitioning

--- Assets/Main/Scripts/Tools/TriggerColliderSpacePartition.cs
+++ Assets/Main/Scripts/Tools/TriggerColliderSpacePartition.cs
@@ -4,59 +4,91 @@ using UnityEngine;
 
 public class TriggerColliderSpacePartition : MonoBehaviour
 {
+    // REVIEW MAGIC NUMBERS!!! problem is getting tile size. leave that for later.
+
     Transform[] transformGrid = new Transform[100];
+    [SerializeField] static TriggerColliderSpacePartitionTile[] gridTiles = new TriggerColliderSpacePartitionTile[100];
     [SerializeField] GameObject gridCollider;
-    [SerializeField] bool enabled;
+    [SerializeField] bool isActive;
     //[SerializeField] Vector3 gridStartPos;
     BoxCollider boxCollider;
     Vector3 transformGridPoint;
-
+    Vector3 gridCoord;
     int counter;
     //Vector3 transformGridPoint;
     // Start is called before the first frame update
     void Start()
     {
-
         // transformGridPoint = gridStartPos;
-        if (enabled)
-        {
-            for (int i = 0; i < 10; i++)
-            {
-                transformGridPoint = transform.position;
-                transformGridPoint.z += i * 100;
+        CreateGridM1();
 
-                for (int j = 0; j < 10; j++)
+        // Debug.Log(boxCollider.bounds.size);
+    }
+
+    private void CreateGridM1()
+    {
+        // we create grid where IDs go from bottom up
+        if (isActive)
+        {
+            for (int i = -5; i < 5; i++)
+            {
+                gridCoord.x = i * 100;
+
+                for (int j = -5; j < 5; j++)
                 {
 
-                    transformGridPoint.x = j * 100 + transform.position.x;
-                    //Debug.Log(transformGridPoint);
-                    var k = Instantiate(gridCollider, transformGridPoint, Quaternion.identity);
+                    gridCoord.z = j * 100;
+                    var k = Instantiate(gridCollider, gridCoord, Quaternion.identity);
                     var b = k.gameObject.GetComponent<TriggerColliderSpacePartitionTile>();
+
+
+
                     b.gridTileID = counter;
+                    gridTiles[counter] = b;
+                    b.tileCoord = gridCoord;
                     counter++;
                     k.transform.parent = transform;
-
-                    transformGrid[i + j] = k.transform;
-
                 }
 
 
             }
 
-
-
-
             boxCollider = gridCollider.GetComponent<BoxCollider>();
         }
-
-       // Debug.Log(boxCollider.bounds.size);
     }
 
-    public static void NearbyGameobjectSearch()
+    public static TriggerColliderSpacePartitionTile ReturnTileByCoord(float x, float y)
     {
+        // this is bad. iterating through whole array. Need to find another way
+        //Debug.Log(x+" coords "+ y);
+        for (int i = 0; i < gridTiles.Length; i++)
+        {
+            if (gridTiles[i].tileCoord.x == x && gridTiles[i].tileCoord.z == y)
+            {
+                // Debug.Log(gridTiles[i].tileCoord+" - coord. id - " + gridTiles[i].gridID);
+                return gridTiles[i];
+            }
+        }
 
+        return null;
     }
 
-   
+    public static TriggerColliderSpacePartitionTile ReturnTileByID(int ID)
+    {
+        // this is bad. iterating through whole array. Need to find another way
+
+        for (int i = 0; i < gridTiles.Length; i++)
+        {
+            if (gridTiles[i].gridTileID == ID)
+            {
+                //Debug.Log(gridTiles[i].tileCoord + " - coord. id - " + gridTiles[i].gridID);
+                return gridTiles[i];
+            }
+        }
+
+        return null;
+    }
+
+
 
 }
--- Assets/Main/Scripts/Tools/TriggerColliderSpacePartitionTile.cs
+++ Assets/Main/Scripts/Tools/TriggerColliderSpacePartitionTile.cs
@@ -8,8 +8,9 @@ public class TriggerColliderSpacePartitionTile : MonoBehaviour
     public int gridTileID;
     public int gridUsersSPPositionerCounter;
     public int gridUsersSPCurrentAmount;
-
+    [SerializeField] public Vector3 tileCoord;
     IGridUserSP[] gridUsersSP=new IGridUserSP[100];
+    public GameObject[] gridUnits = new GameObject[100];
     //public GameObject[] gridUsersSP = new GameObject[100];
     public int[] gridUnitIDs = new int[100];
     BoxCollider boxCollider;
@@ -100,4 +101,185 @@ public class TriggerColliderSpacePartitionTile : MonoBehaviour
         return gridUsersSP;
     }
 
+    public static TriggerColliderSpacePartitionTile[] GetNeighbors(TriggerColliderSpacePartitionTile originGridTile)
+    {
+        var neighbors = new TriggerColliderSpacePartitionTile[9];//we can only have max of  8 neighbors on the cube grid plus current tile
+        /*
+        table[i - 1][j - 1]
+        table[i - 1][j]
+        table[i - 1][j + 1]
+
+        table[i][j - 1]
+        table[i][j + 1]
+
+        table[i + 1][j - 1]
+        table[i + 1][j]
+        table[i + 1][j + 1]
+        */
+
+        var x = originGridTile.tileCoord.x;
+        var y = originGridTile.tileCoord.z;
+
+        neighbors[8] = TriggerColliderSpacePartition.ReturnTileByCoord(x, y);
+
+        var temp_x = x - 100;
+        var temp_y = y - 100;
+        neighbors[0] = TriggerColliderSpacePartition.ReturnTileByCoord(temp_x, temp_y);
+
+        temp_x = x - 100;
+        temp_y = y;
+        neighbors[1] = TriggerColliderSpacePartition.ReturnTileByCoord(temp_x, temp_y);
+
+        temp_x = x - 100;
+        temp_y = y + 100;
+        neighbors[2] = TriggerColliderSpacePartition.ReturnTileByCoord(temp_x, temp_y);
+
+        temp_x = x;
+        temp_y = y - 100;
+        neighbors[3] = TriggerColliderSpacePartition.ReturnTileByCoord(temp_x, temp_y);
+
+        temp_x = x;
+        temp_y = y + 100;
+        neighbors[4] = TriggerColliderSpacePartition.ReturnTileByCoord(temp_x, temp_y);
+
+        temp_x = x + 100;
+        temp_y = y - 100;
+        neighbors[5] = TriggerColliderSpacePartition.ReturnTileByCoord(temp_x, temp_y);
+
+        temp_x = x + 100;
+        temp_y = y;
+        neighbors[6] = TriggerColliderSpacePartition.ReturnTileByCoord(temp_x, temp_y);
+
+        temp_x = x + 100;
+        temp_y = y + 100;
+        neighbors[7] = TriggerColliderSpacePartition.ReturnTileByCoord(temp_x, temp_y);
+
+        return neighbors;
+    }
+
+    public static TriggerColliderSpacePartitionTile[] GetNeighbors(int tileID)
+    {
+        var originGridTile = TriggerColliderSpacePartition.ReturnTileByID(tileID);
+        var neighbors = new TriggerColliderSpacePartitionTile[9];//we can only have max of  8 neighbors on the cube grid plus current tile
+
+        var x = originGridTile.tileCoord.x;
+        var y = originGridTile.tileCoord.z;
+
+        //Debug.Log(x);
+        //Debug.Log(y);
+
+        neighbors[8] = TriggerColliderSpacePartition.ReturnTileByCoord(x, y);
+
+        var temp_x = x - 100;
+        var temp_y = y - 100;
+        neighbors[0] = TriggerColliderSpacePartition.ReturnTileByCoord(temp_x, temp_y);
+
+        temp_x = x - 100;
+        temp_y = y;
+        neighbors[1] = TriggerColliderSpacePartition.ReturnTileByCoord(temp_x, temp_y);
+
+        temp_x = x - 100;
+        temp_y = y + 100;
+        neighbors[2] = TriggerColliderSpacePartition.ReturnTileByCoord(temp_x, temp_y);
+
+        temp_x = x;
+        temp_y = y - 100;
+        neighbors[3] = TriggerColliderSpacePartition.ReturnTileByCoord(temp_x, temp_y);
+
+        temp_x = x;
+        temp_y = y + 100;
+        neighbors[4] = TriggerColliderSpacePartition.ReturnTileByCoord(temp_x, temp_y);
+
+        temp_x = x + 100;
+        temp_y = y - 100;
+        neighbors[5] = TriggerColliderSpacePartition.ReturnTileByCoord(temp_x, temp_y);
+
+        temp_x = x + 100;
+        temp_y = y;
+        neighbors[6] = TriggerColliderSpacePartition.ReturnTileByCoord(temp_x, temp_y);
+
+        temp_x = x + 100;
+        temp_y = y + 100;
+        neighbors[7] = TriggerColliderSpacePartition.ReturnTileByCoord(temp_x, temp_y);
+
+        return neighbors;
+    }
+
+    public static GameObject[] GetAllNeighborUnits(int tileID)
+    {
+        // another "super efficient" function. gotta redesign this
+        var originGridTile = TriggerColliderSpacePartition.ReturnTileByID(tileID);
+
+        TriggerColliderSpacePartitionTile[] relevantTiles = TriggerColliderSpacePartitionTile.GetNeighbors(originGridTile);
+        int tileUnitCounter = 0;
+        int currentAddPos = 0;
+
+        for (int i = 0; i < relevantTiles.Length; i++)
+        {
+            for (int j = 0; j < relevantTiles[i].gridUnits.Length; j++)
+            {
+                if (relevantTiles[i].gridUnits[j] != null)
+                {
+                    tileUnitCounter++;
+                }
+
+            }
+        }
+        //Debug.Log(tileUnitCounter);
+        GameObject[] tempArray = new GameObject[tileUnitCounter];
+
+        for (int i = 0; i < relevantTiles.Length; i++)
+        {
+            for (int j = 0; j < relevantTiles[i].gridUnits.Length; j++)
+            {
+                if (relevantTiles[i].gridUnits[j] != null)
+                {
+                    //Debug.Log(i+j);
+                    tempArray[currentAddPos] = relevantTiles[i].gridUnits[j];
+                    currentAddPos++;
+                }
+
+            }
+        }
+        //Debug.Log(tempArray.Length);
+        return tempArray;
+
+    }
+
+    public static GameObject[] GetAllNeighborUnits(TriggerColliderSpacePartitionTile originGridTile)
+    {
+        // another "super efficient" function. gotta redesign this
+
+        TriggerColliderSpacePartitionTile[] relevantTiles = TriggerColliderSpacePartitionTile.GetNeighbors(originGridTile);
+        int tileUnitCounter = 0;
+
+
+        for (int i = 0; i < relevantTiles.Length; i++)
+        {
+            for (int j = 0; j < relevantTiles[i].gridUnits.Length; j++)
+            {
+                if (relevantTiles[i].gridUnits[j] != null)
+                {
+                    tileUnitCounter++;
+                }
+
+            }
+        }
+        GameObject[] tempArray = new GameObject[tileUnitCounter];
+
+        for (int i = 0; i < relevantTiles.Length; i++)
+        {
+            for (int j = 0; j < relevantTiles[i].gridUnits.Length; j++)
+            {
+                if (relevantTiles[i].gridUnits[j] != null)
+                {
+                    tempArray[i + j] = relevantTiles[i].gridUnits[j];
+                }
+
+            }
+        }
+
+        return tempArray;
+
+    }
 }

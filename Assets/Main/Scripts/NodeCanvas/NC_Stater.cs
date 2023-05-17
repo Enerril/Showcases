904a851533f839f6f24a935c5e4a528e11b15253
SpiralBeam <32187189+Enerril@users.noreply.github.com>
2023-05-17 07:30:11 +0300

    import first 3d model. import node canvas

---
+++ Assets/Main/Scripts/NodeCanvas/NC_Stater.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NC_Stater : MonoBehaviour
+{
+    // test values for nodeCanvas to test out behaviorTrees
+
+    public bool isIdle { get; set; }  
+    public bool isRunning { get; set; }
+    public bool isPaused { get; set; }
+    public bool isStopped { get; set; }
+
+
+
+}
